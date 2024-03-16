984fd5225d7afe639d9348d81ce76a0fc5d53b9d
HafizMuhammadAlIkhsan <hafiz.muhammad.tif23@polban.ac.id>
2024-03-16 23:17:33 +0700

    menambahkah modul menuVisiMisi, modul VisiMisi, dan menambahkan karakter 'spasi' pada enkripsi

--- 231511077/231511077.cpp
+++ 231511077/231511077.cpp
@@ -1,42 +1,88 @@
 #include "231511077.h"
+#include "../231511076/231511076.h"
 
 void HapusLayar() {
     system("cls");
 }
 
+void menuVisiMisi(Pengguna pengguna) {
+    int menu;
+
+    std::cout << "==================== "<< "Selamat datang " << pengguna.nama <<" ====================\n" << std::endl;
+    std::cout << "Menu Melihat Visi dan Misi Calon" << std::endl;
+    std::cout << "\n1. Lihat Visi dan Misi Calon Hafiz Muhammad Al Ikhsan" << std::endl;
+    std::cout << "2. Lihat Visi dan Misi Calon Fauzan Rizky Ramadhan" << std::endl;
+    std::cout << "3. Lihat Visi dan Misi Calon Abyan Dzaky Pratama" << std::endl;
+    std::cout << "4. Kembali ke tampilan utama." << std::endl;
+
+    std::cin >> menu;
+
+    switch (menu) {
+        case 1:
+            VisiMisi(menu);
+            HapusLayar();
+            menuVisiMisi(pengguna);
+        break;
+        case 2:
+            VisiMisi(menu);
+            HapusLayar();
+            menuVisiMisi(pengguna);
+        break;
+        case 3:
+            VisiMisi(menu);
+            HapusLayar();
+            menuVisiMisi(pengguna);
+        break;
+        case 4:
+            menuUtama(pengguna);
+        break;
+        default:
+            std::cout << "Tolong pilih opsi sesuai dengan yang ditampilkan di atas." << std::endl;
+            menuVisiMisi(pengguna);
+    }
+}
+
 void VisiMisi(int calon) {
-    clearScreen();
+    HapusLayar();
     if (calon == 1) {
-        std::cout << "===========================================================================" << std::endl;
-        std::cout << "|                             VISI DAN MISI                               |" << std::endl;
-        std::cout << "===========================================================================" << std::endl;
-        std::cout << "| Calon Ketua Hafiz Muhammad Al Ikhsan                                    |" << std::endl;
-        std::cout << "|                                                                         |" << std::endl;
-        std::cout << "| Visi:                Mewujudkan kemajuan dan kesejahteraan              |" << std::endl;
-        std::cout << "| Misi:             1. Mengembangkan potensi anggota tikus                |" << std::endl;
-        std::cout << "|                   2. Menjalin kerjasama yang baik dengan pihak alien    |" << std::endl;
-        std::cout << "|                   3. Makan siang gratis                                 |" << std::endl;
-        std::cout << "===========================================================================" << std::endl;
+        std::cout << "==================================================================================================================" << std::endl;
+        std::cout << "|                          CALON PASANGAN HAFIZ MUHAMMAD AL IKHSAN & ELSA MONIKA SINAGA                          |" << std::endl;
+        std::cout << "------------------------------------------------------------------------------------------------------------------" << std::endl;
+        std::cout << "|                                               VISI DAN MISI                                                    |" << std::endl;
+        std::cout << "==================================================================================================================" << std::endl;
+        std::cout << "|                                                                                                                |" << std::endl;
+        std::cout << "| Visi:             Membangun kampus yang berdaya saing tinggi, berbudaya, serta mendorong                       |" << std::endl;
+        std::cout << "|                   keterlibatan aktif mahasiswa dalam pembangunan kampus yang berkelanjutan.                    |" << std::endl;
+        std::cout << "|                                                                                                                |" << std::endl;
+        std::cout << "| Misi:             1. Menjalin komunikasi dan kerjasama dari dalam lingkungan internal dan eksternal Kampus.    |" << std::endl;
+        std::cout << "|                   2. Menyelenggarakan program kerja yang solutif bagi mahasiswa.                               |" << std::endl;
+        std::cout << "|                   3. Makan siang gratis.                                                                       |" << std::endl;
+        std::cout << "==================================================================================================================" << std::endl;
     } else if (calon == 2) {
-        std::cout << "==============================================================================" << std::endl;
-        std::cout << "|                                VISI DAN MISI                               |" << std::endl;
-        std::cout << "==============================================================================" << std::endl;
-        std::cout << "| Calon Ketua Fauzan Rizky Ramadhan                                          |" << std::endl;
-        std::cout << "|                                                                            |" << std::endl;
-        std::cout << "| Visi:               Menjadi agen negara                                    |" << std::endl;
-        std::cout << "| Misi:            1. Mewujudkan kampus penuh donat                          |" << std::endl;
-        std::cout << "|                  2. Membangun hubungan yang kuat dengan masyarakat         |" << std::endl;
-        std::cout << "==============================================================================" << std::endl;
+        std::cout << "===============================================================================================================" << std::endl;
+        std::cout << "|                        CALON PASANGAN FAUZAN RIZKY RAMADHAN & AULIA PUTRI RAHMADHANI                        |" << std::endl;
+        std::cout << "---------------------------------------------------------------------------------------------------------------" << std::endl;
+        std::cout << "|                                              VISI DAN MISI                                                  |" << std::endl;
+        std::cout << "===============================================================================================================" << std::endl;
+        std::cout << "|                                                                                                             |" << std::endl;
+        std::cout << "| Visi:               Menjadi agen negara                                                                     |" << std::endl;
+        std::cout << "|                                                                                                             |" << std::endl;
+        std::cout << "| Misi:            1. Terlaksananya pelatihan pelatiahan yang meningkatkan kualiatas Sumber Daya Mahasiswa.   |" << std::endl;
+        std::cout << "|                  2. Membangun hubungan yang kuat dengan masyarakat.                                         |" << std::endl;
+        std::cout << "|                  3. Program berbagi donat gratis untuk seluruh mahasiswa.                                   |" << std::endl;
+        std::cout << "===============================================================================================================" << std::endl;
     } else if (calon == 3) {
-        std::cout << "==============================================================================" << std::endl;
-        std::cout << "|                                VISI DAN MISI                               |" << std::endl;
-        std::cout << "==============================================================================" << std::endl;
-        std::cout << "| Calon Ketua Fauzan Rizky Ramadhan                                          |" << std::endl;
-        std::cout << "|                                                                            |" << std::endl;
-        std::cout << "| Visi:               Menjadi agen negara                                    |" << std::endl;
-        std::cout << "| Misi:            1. Mewujudkan kampus penuh donat                          |" << std::endl;
-        std::cout << "|                  2. Membangun hubungan yang kuat dengan masyarakat         |" << std::endl;
-        std::cout << "==============================================================================" << std::endl;
+        std::cout << "================================================================================" << std::endl;
+        std::cout << "|               CALON PASANGAN ABYAN DZAKY PRATAMA & UDIN RACING               |" << std::endl;
+        std::cout << "--------------------------------------------------------------------------------" << std::endl;
+        std::cout << "|                                VISI DAN MISI                                 |" << std::endl;
+        std::cout << "================================================================================" << std::endl;
+        std::cout << "|                                                                              |" << std::endl;
+        std::cout << "| Visi:               Menjadi agen negara                                      |" << std::endl;
+        std::cout << "|                                                                              |" << std::endl;
+        std::cout << "| Misi:            1. Meningkatnya produktifitas dan kreatifitas mahasiswa.    |" << std::endl;
+        std::cout << "|                  2. Membangun hubungan yang kuat dengan masyarakat.          |" << std::endl;
+        std::cout << "================================================================================" << std::endl;
     }
 
     std::cout << "Tekan enter untuk kembali." << std::endl;
@@ -44,24 +90,30 @@ void VisiMisi(int calon) {
     std::cin.get();
 }
 
-char karakter[] = {
+std::string enkripsi(std::string plaintext) 
+{
+    char karakter[] = {
     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
     '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '{', '}', '[', ']', '<', '>', '.', ',', ';',
-    '"', '\'', '`', '\\', '/', '?', ':', '~', '|',
-};
+    '"', '\'', '\\', '/', '?', ':', '~', '|', ' '
+    };
 
-int kunciMatriks[2][2] = {{2, 1}, {3, 4}};
+    int modulus = sizeof(karakter);
+    int kunciMatriks[2][2] = {{2, 1}, {3, 4}};
 
-std::string enkripsi(std::string plaintext) 
-{
     std::string ciphertext = "";
+
+    if (plaintext.size() % 2 == 1)
+    {
+        plaintext = plaintext + ' ';
+    }
 
     for (int i = 0; i < plaintext.size(); i = i + 2) 
     {
         int x = 0, y = 0;
-        for (int j = 0; j < sizeof(karakter); ++j) 
+        for (int j = 0; j < modulus; ++j) 
         {
             if (karakter[j] == plaintext[i]) 
             {
@@ -69,7 +121,7 @@ std::string enkripsi(std::string plaintext)
                 break;
             }
         }
-        for (int j = 0; j < sizeof(karakter); ++j) 
+        for (int j = 0; j < modulus; ++j) 
         {
             if (karakter[j] == plaintext[i + 1]) 
             {
@@ -78,12 +130,85 @@ std::string enkripsi(std::string plaintext)
             }
         }
 
-        int encX = (kunciMatriks[0][0] * x + kunciMatriks[0][1] * y) % sizeof(karakter);
-        int encY = (kunciMatriks[1][0] * x + kunciMatriks[1][1] * y) % sizeof(karakter);
+        int encX = (kunciMatriks[0][0] * x + kunciMatriks[0][1] * y) % modulus;
+        int encY = (kunciMatriks[1][0] * x + kunciMatriks[1][1] * y) % modulus;
 
         ciphertext = ciphertext + karakter[encX];
         ciphertext = ciphertext + karakter[encY];
     }
 
     return ciphertext;
+}
+
+// std::string decrypt(std::string ciphertext, int kunciMatriks[2][2]) 
+// {
+//     char karakter[] = {
+//     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+//     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+//     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+//     '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '{', '}', '[', ']', '<', '>', '.', ',', ';',
+//     '"', '\'', '\\', '/', '?', ':', '~', '|', ' '
+//     };
+
+//     std::string plaintext = "";
+//     int modulus = sizeof(karakter);
+
+//     inversMatriks(kunciMatriks, modulus);
+
+//     for (int i = 0; i < ciphertext.size(); i = i + 2) 
+//     {
+//         int x = 0, y = 0;
+//         for (int j = 0; j < modulus; ++j) 
+//         {
+//             if (karakter[j] == ciphertext[i]) 
+//             {
+//                 x = j;
+//                 break;
+//             }
+
+//         }
+
+//         for (int j = 0; j < modulus; ++j) 
+//         {
+//             if (karakter[j] == ciphertext[i + 1]) 
+//             {
+//                 y = j;
+//                 break;
+//             }
+//         }
+
+//         int encX = (kunciMatriks[0][0] * x + kunciMatriks[0][1] * y) % modulus;
+//         int encY = (kunciMatriks[1][0] * x + kunciMatriks[1][1] * y) % modulus;
+
+
+//         plaintext = plaintext + karakter[encX];
+
+//         plaintext = plaintext + karakter[encY];
+//     }
+
+//     if(plaintext.back() == ' ')
+//     {
+//         plaintext.pop_back();
+//     }
+
+//     return plaintext;
+// }
+
+int main () {
+    // int pilihan;
+
+    // Pengguna pengguna;
+    // pengguna.nama = "Hafiz";
+
+    // std::cout << "Lihat Visi Misi\n" ;
+    // std::cin >> pilihan;
+
+    // menuVisiMisi(pengguna);
+
+    std::string plaintext;
+    std::cout << "Masukkan teks yang ingin dienkripsi: ";
+    std::getline(std::cin, plaintext);
+
+    std::cout << "Ciphertext: " << enkripsi(plaintext);
+
 }
--- 231511077/231511077.h
+++ 231511077/231511077.h
@@ -1,10 +1,14 @@
 #ifndef ENKRIPSI_H
 #define ENKRIPSI_H
 
+#include "../pengguna.h"
+#include "../231511076/231511076.h"
 #include <iostream>
 #include <string>
 
 void HapusLayar();
+
+void menuVisiMisi(Pengguna pengguna);
 
 void VisiMisi(int calon);
 
